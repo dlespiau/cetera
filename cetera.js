b70911deb6cf51f12229d667a162ba60f8e96480
Damien Lespiau <damien.lespiau@gmail.com>
2020-12-27 18:57:54 +0000

    [core] dist

--- cetera.js
+++ cetera.js
@@ -1,3 +1,7 @@
+function dist(x1, y1, x2, y2) {
+  return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+}
+
 function map(x, istart, istop, ostart, ostop) {
   return ostart + (ostop - ostart) * ((x - istart) / (istop - istart));
 }
@@ -11,6 +15,7 @@ function degrees(radians) {
 }
 
 module.exports = {
+  dist,
   map,
   radians,
   degrees,
